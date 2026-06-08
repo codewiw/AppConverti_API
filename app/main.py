c64dbab8424871fd411bd5aadacea5de58af4df5
Willian Ramalho <willian.ramalho42@gmail.com>
2026-06-07 21:21:51 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ from app.api import auth, favorites
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="API Converti", version="1.0.0")
-
-app.include_router(auth.router) 
+app.include_router(auth.router)
+app.include_router(favorites.router)
 
 @app.get("/")
 def health_check():
